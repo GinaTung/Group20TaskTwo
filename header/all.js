eb906782ef045ee123c9640b008883c2b06c97b5
yuling <gb2013429@gmail.com>
2024-05-26 21:27:43 +0800

    add 四個角 + 動畫

--- header/all.js
+++ header/all.js
@@ -1,4 +1,12 @@
-// $('.open').click(function() {
-//     $('.header-btn').toggleClass('open');
-//     $('.header-nav').toggleClass();
-//   })
+$(".header-btn").click(function() {
+    $(".header-menu").toggleClass("close");
+    $(".header-close").toggleClass("close");
+    $(".header-nav").toggleClass("open");
+    // 如果列表是展開的，則使用slideDown方法緩慢展開
+    if ($(".header-nav").hasClass("open")) {
+        $(".header-nav").hide().slideDown(1000); // 5000毫秒即5秒
+    } else {
+        // 如果列表是收起的，則使用slideUp方法緩慢收起
+        $(".header-nav").slideUp(1000); // 5000毫秒即5秒
+    }
+  });
